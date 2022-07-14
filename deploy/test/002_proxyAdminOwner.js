d81143b140249e9b172f9e762b65f2979e562319
Vladislav Komissarov <mastyf@gmail.com>
2022-07-14 17:47:51 +0300

    avalanche new treasury deploy

--- deploy/test/002_proxyAdminOwner.js
+++
@@ -1,8 +0,0 @@
-const { migration } = require('../../scripts/deploy');
-
-module.exports = migration(async (deployer) => {
-  const timelock = await deployer.artifacts.readDeploy('Timelock');
-
-  return deployer.transferProxyAdminOwnership(timelock.address);
-});
-module.exports.tags = ['Test'];
