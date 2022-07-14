d80e455b595a08740d9cc4f1efa906212be751e4
artur <amamedbekov@tokenomica.com>
2022-07-14 17:07:32 +0300

    remove priceFeed named accounts from hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -192,20 +192,6 @@ module.exports = {
     consumer3: {
       '': 4,
     },
-    priceFeed: {
-      '': '0x0000000000000000000000000000000000000000',
-      'main': '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
-      'ropsten': '0x64125B3eDF8C778c0814A6cd368a133764B1e156',
-      'optimistic': '0x13e3Ee699D1909E989722E753853AE30b17e08c5',
-      'bsc': '0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE',
-      'bscTest': '0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526',
-      'polygon': '0xAB594600376Ec9fD91F8e885dADF0CE036862dE0',
-      'moonriver': '0x3f8BFbDc1e79777511c00Ad8591cef888C2113C1',
-      43113: '0x5498BB86BC934c8D34FDA08E81D444153d0D06aD',
-      'avalancheTest': '0x5498BB86BC934c8D34FDA08E81D444153d0D06aD',
-      'avalanche': '0x0A77230d17318075983913bC2145DB16C7366156',
-      'mumbai': '0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada',
-    },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
