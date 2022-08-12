a54b0d2d8ef9b45156fd591332bd7ac890368bc5
Vladislav Komissarov <mastyf@gmail.com>
2022-08-12 16:56:16 +0300

    howManyOwnersDecide = 2 for Multisig

--- deploy/lite/001_governance/002_GovernorInitialize.js
+++ deploy/lite/001_governance/002_GovernorInitialize.js
@@ -5,7 +5,8 @@ module.exports = migration(async (deployer) => {
   const owners = JSON.parse(process.env[`${hardhat.network.name}_MULTISIG`] ?? '[]');
   if (owners.length === 0) throw new Error('Invalid owners count');
 
-  const howManyOwnersDecide = Math.floor(owners.length * 0.75);
+  // const howManyOwnersDecide = Math.floor(owners.length * 0.75);
+  const howManyOwnersDecide = 2;
   await deployer.execute('GovernorMultisig', 'transferOwnershipWithHowMany', [
     owners,
     howManyOwnersDecide >= 1 ? howManyOwnersDecide : 1,
