d4fed51d9410e9940e719c19ec112fb1d51b1931
artur <amamedbekov@tokenomica.com>
2022-09-28 15:46:25 +0300

    smart trade swap handler fix

---
+++ deploy/dev/004_smartTrade/002_swapHandler/002_storageInfo.js
@@ -0,0 +1,15 @@
+const { migration } = require('../../../../scripts/deploy');
+const { utils } = require('ethers');
+
+module.exports = migration(async (deployer) => {
+  const handler = await deployer.artifacts.readDeploy('SmartTradeSwapHandler');
+
+  console.info(
+    `Storage.setBool("${utils.solidityKeccak256(
+      ['string', 'address'],
+      ['DFH:Contract:SmartTrade:allowedHandler:', handler.address],
+    )}", 1)`,
+  );
+  console.info(`Storage.setUint("${utils.keccak256(utils.toUtf8Bytes('DFH:Fee:Automate:SmartTrade'))}", 100000000)`);
+});
+module.exports.tags = ['DFH', 'Dev', 'Protocol', 'NonUpgradable'];
